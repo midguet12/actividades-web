c6554ab13fddad074cff0c15ce6340ffba106a21
midguet12 <midguet12@hotmail.com>
2023-10-20 16:52:44 -0600

    function correction

--- actividad 9/foco.js
+++ actividad 9/foco.js
@@ -1,32 +1,33 @@
 window.addEventListener('load', function(){
     foco();
 })
+function foco(){
+    document.querySelector('body').classList.add('oscuridad');
+    const titulo = document.createElement('h1');
+    titulo.classList.add("titulo");
 
-document.querySelector('body').classList.add('oscuridad');
-const titulo = document.createElement('h1');
-titulo.classList.add("titulo");
+    const anio = new Date().getFullYear();
 
-const anio = new Date().getFullYear();
+    titulo.innerText = `Desarrollo de sistemas Web ${anio}`;
 
-titulo.innerText = `Desarrollo de sistemas Web ${anio}`;
+    document.body.appendChild(titulo);
 
-document.body.appendChild(titulo);
+    const img = document.createElement("img");
+    img.classList.add("foco");
+    img.setAttribute('src', 'images/foco-apagado.jpg')
 
-const img = document.createElement("img");
-img.classList.add("foco");
-img.setAttribute('src', 'images/foco-apagado.jpg')
+    img.addEventListener('click', () =>{
+        if(img.src.indexOf("apagado") > 0){
+            img.setAttribute('src', 'images/foco-prendido.jpg');
+            document.body.classList.remove('oscuridad');
+            document.body.classList.add('luz');
 
-img.addEventListener('click', () =>{
-    if(img.src.indexOf("apagado") > 0){
-        img.setAttribute('src', 'images/foco-prendido.jpg');
-        document.body.classList.remove('oscuridad');
-        document.body.classList.add('luz');
+        } else{
+            img.setAttribute('src', 'images/foco-apagado.jpg');
+            document.body.classList.remove('luz');
+            document.body.classList.add('oscuridad');
+        }
+    });
 
-    } else{
-        img.setAttribute('src', 'images/foco-apagado.jpg');
-        document.body.classList.remove('luz');
-        document.body.classList.add('oscuridad');
-    }
-});
-
-document.body.appendChild(img);
+    document.body.appendChild(img);
+}
